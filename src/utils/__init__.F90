22d5081c4200c1a0de8a52f1df0b5f0fb97b7893
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2026-08-10 18:26:30 +0200

    Add alloc to fix name collision for character in allocate for GNU compiler

--- src/utils/__init__.F90
+++ src/utils/__init__.F90
@@ -1,8 +1,18 @@
+#include "../inc/init.inc"
+#include "../inc/utils.inc"
+
 module pytran_utils
     implicit none
 
     private
     public :: str
+
+#define _PROC _UNARY_OP(_OP)
+#define _TYPE_IDS _CHARACTER
+#define _OP alloc
+#include "../inc/iface.inc"
+#undef _TYPE_IDS
+#undef _PROC
 
 contains
     pure function str(arg) result(res)
@@ -14,4 +24,12 @@ contains
         write(buf, '(i0)') arg
         res = trim(buf)
     end function str
+
+
+#define _TYPE_IDS _CHARACTER
+#define _FILE "../utils/alloc.inc"
+#include "../inc/types.inc"
+#undef _FILE
+#undef _TYPE_IDS
+
 end module pytran_utils
