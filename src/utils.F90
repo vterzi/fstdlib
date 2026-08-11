d6fc7d1dfc70f51f59645ffa8cfd2e928702fee8
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2026-08-11 12:02:40 +0200

    Revert "Add alloc to fix name collision for character in allocate for GNU compiler"
    This reverts commit 22d5081c4200c1a0de8a52f1df0b5f0fb97b7893.

--- src/utils.F90
+++ src/utils.F90
@@ -1,18 +1,8 @@
-#include "../inc/init.inc"
-#include "../inc/utils.inc"
-
 module pytran_utils
     implicit none
 
     private
     public :: str
-
-#define _PROC _UNARY_OP(_OP)
-#define _TYPE_IDS _CHARACTER
-#define _OP alloc
-#include "../inc/iface.inc"
-#undef _TYPE_IDS
-#undef _PROC
 
 contains
     pure function str(arg) result(res)
@@ -24,12 +14,4 @@ contains
         write(buf, '(i0)') arg
         res = trim(buf)
     end function str
-
-
-#define _TYPE_IDS _CHARACTER
-#define _FILE "../utils/alloc.inc"
-#include "../inc/types.inc"
-#undef _FILE
-#undef _TYPE_IDS
-
 end module pytran_utils
